b121c2bf3caffbc461cef311bcacb68e4d947466
Dan Chen <chendbox@gmail.com>
2022-11-13 13:40:46 -0500

    initiate commits

---
+++ test.py
@@ -0,0 +1,13 @@
+import sys
+import requests
+import math
+from os import rename
+
+name = input("your name? ")
+print("hello,", name)
+
+
+## shift + option  + F  -> formating the codes
+## contro + option + N  -> excute code
+## if run input, don't use code runer, just right click and select run file terminal 
+
